32ce2491e3418af835cd47c4239c67b1fd2ce9f1
L-E-Crane <107731061+L-E-Crane@users.noreply.github.com>
2023-02-16 13:58:32 -0800

    added unit choice

--- src/index.js
+++ src/index.js
@@ -36,10 +36,13 @@ function showTemperature(response) {
   let currentTemperature = document.querySelector("#current-temperature");
   let weatherDescription = document.querySelector("#current-description");
   let currentWindspeed = document.querySelector("#current-windspeed");
+
+  celsiusTemperature = Math.round(response.data.main.temp);
+
   currentCity.innerHTML = searchedCity;
   currentTemperature.innerHTML = `${temperature}`;
   weatherDescription.innerHTML = description;
-  currentWindspeed.innerHTML = `${windspeed} km/h`;
+  currentWindspeed.innerHTML = `Wind: ${windspeed} km/h`;
   currentIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -60,5 +63,26 @@ function submitSearch(event) {
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitSearch);
+
+////////
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 citySearch("Portland");
